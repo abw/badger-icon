93b66e31308da1ecff1037d536a460e40337f2c5
Andy Wardley <abw@wardley.org>
2024-05-05 11:27:43 +0100

    Better error handling

--- lib/components/Icon.jsx
+++ lib/components/Icon.jsx
@@ -3,6 +3,7 @@ import Library      from '../Library.js'
 import SVGIcon      from '../SVG/Icon.jsx'
 import { Themed }   from '../Theme.js'
 import { resolveIconData, prepareIconProps, classes, splitSpace } from '../utils/index.js'
+import { fail } from '@abw/badger-utils'
 
 // TODO: fixedWidth
 
@@ -15,22 +16,27 @@ export const Icon = ({
   if (! name) {
     return <SVGIcon {...prepareIconProps(props)} />
   }
-  const names = splitSpace(name)
-  return names.length === 1
-    ? <SVGIcon
-        {...props}
-        {...resolveIconData(name, library, props)}
-      />
-    : <div className={classes(iconsClass, props.className)}>
-        { names.map(
-          name =>
-            <SVGIcon
-              key={name}
-              {...props}
-              {...resolveIconData(name, library, props)}
-            />
-        )}
-      </div>
+  try {
+    const names = splitSpace(name)
+    return names.length === 1
+      ? <SVGIcon
+          {...props}
+          {...resolveIconData(name, library, props)}
+        />
+      : <div className={classes(iconsClass, props.className)}>
+          { names.map(
+            name =>
+              <SVGIcon
+                key={name}
+                {...props}
+                {...resolveIconData(name, library, props)}
+              />
+          )}
+        </div>
+  }
+  catch(e) {
+    fail(`Badger Icon failed to render icon "${name}". ${e.message}`)
+  }
 }
 
 export default Themed(Icon, 'Icon')
